b75e8c89ef03eb3389990d573b67d12da34833b8
kamczill <kam134@interia.eu>
2023-12-13 09:49:15 +0100

    fix

--- src/pages/VisitDetailsPage/VisitDetailsPage.jsx
+++ src/pages/VisitDetailsPage/VisitDetailsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { IoLocationOutline, IoCallOutline, IoMailOutline, IoCalendarClearOutline, IoHeartOutline } from "react-icons/io5";
 import InfoItem from '../../components/InfoItem';
 import mapImg from './../../assets/map.png'
@@ -7,6 +7,9 @@ import { useParams } from 'react-router-dom';
 
 const VisitDetailsPage = () => {
   const { id } = useParams()
+  const [isDetailsOpen, setIsDetailsOpen] = useState(true)
+
+  const handleToggleDetails = () => setIsDetailsOpen(!isDetailsOpen)
 
   return (
     <div className='bg-[#F6F6F6] min-h-[50vh] w-full flex flex-col items-center '>
@@ -14,7 +17,7 @@ const VisitDetailsPage = () => {
             <div className='bg-white p-6 w-full'>
                 <h2 className='text-2xl font-bold mb-8'>Szczegóły wizyty</h2>
                 <article className={`flex flex-col gap-4 items-center w-full md:flex-row md:items-start lg:gap-8 pb-10 `}>
-                    <div className='bg-[#6D4646] w-[200px] h-[200px]'></div>
+                    <div className='bg-[#6D4646] w-[200px] h-[200px] shrink-0'></div>
                     <div className='flex flex-col gap-2 text-lg'>
                         <div className='flex flex-col gap-2 mb-4'>
                           <h3 className='font-bold text-xl'>lek. Laura Molińska</h3>
@@ -26,10 +29,17 @@ const VisitDetailsPage = () => {
                         <InfoItem icon={<IoCalendarClearOutline className='text-2xl' />} text="12 stycznia 2024 godz. 16:00" />
                         <InfoItem icon={<IoHeartOutline className='text-2xl' />} text="konsultacja logopedyczna" />
                         <div className='flex flex-col gap-4 mt-4 w-full lg:flex-row'>
-                            <button className='bg-[#303030] text-white py-2 px-4 rounded-lg lg:px-0 lg:w-[148px]'>Szczegóły</button>
+                            <button onClick={handleToggleDetails} className={`${isDetailsOpen ? 'bg-[#5a5959]' : 'bg-[#303030]'} text-white py-2 px-4 rounded-lg lg:px-0 lg:w-[148px]`}>Szczegóły</button>
                             <button className='bg-[#303030] text-white py-2 px-4 rounded-lg lg:px-0 lg:w-[148px]'>Przełóż wizytę</button>
                             <button className='bg-[#303030] text-white py-2 px-4 rounded-lg lg:px-0 lg:w-[148px]'>Odwołaj wizytę</button>
                         </div>
+                        {
+                            isDetailsOpen && (
+                                <div>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit suscipit obcaecati eaque alias ducimus, nostrum, labore iste possimus, explicabo quidem expedita quos odio. Quo deleniti animi autem, similique minima incidunt?</p>
+                                </div>
+                            )
+                        }
                     </div>
                 </article>
             </div>
