91957c579a5c0f74ea77599c2713fbd27e2a1490
kamczill <kam134@interia.eu>
2023-12-13 09:50:45 +0100

    update

--- src/pages/VisitDetailsPage/VisitDetailsPage.jsx
+++ src/pages/VisitDetailsPage/VisitDetailsPage.jsx
@@ -12,7 +12,7 @@ const VisitDetailsPage = () => {
   const handleToggleDetails = () => setIsDetailsOpen(!isDetailsOpen)
 
   return (
-    <div className='bg-[#F6F6F6] min-h-[50vh] w-full flex flex-col items-center '>
+    <div className='bg-[#F6F6F6] min-h-[60vh]  w-full flex flex-col items-center '>
         <div className='py-8 max-w-[1400px] px-6 w-full md:px-8 flex flex-col gap-8 md:flex-row md:items-start lg:px-14 '>
             <div className='bg-white p-6 w-full'>
                 <h2 className='text-2xl font-bold mb-8'>Szczegóły wizyty</h2>
